7a7067009c8279d693cfd311c3847244d0090e9a
Luksa Kraljevic <kraljevic@cirus.dhz.hr>
2014-03-28 11:59:04 +0100

    rijesio getZadnjiPodatakPoProgramu

--- src/java/dhz/skz/aqdb/facades/PodatakFacade.java
+++ src/java/dhz/skz/aqdb/facades/PodatakFacade.java
@@ -3,23 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dhz.skz.aqdb.facades;
-
 
 import dhz.skz.likz.aqdb.entity.IzvorPodataka;
 import dhz.skz.likz.aqdb.entity.IzvorPodataka_;
+import dhz.skz.likz.aqdb.entity.NivoValidacije;
 import dhz.skz.likz.aqdb.entity.Podatak;
+import dhz.skz.likz.aqdb.entity.Podatak_;
 import dhz.skz.likz.aqdb.entity.ProgramMjerenja;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Tuple;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 /**
@@ -28,6 +30,7 @@ import javax.persistence.criteria.Root;
  */
 @Stateless
 public class PodatakFacade extends AbstractFacade<Podatak> {
+
     @PersistenceContext(unitName = "CitacModulPU")
     private EntityManager em;
     private static final Logger log = Logger.getLogger(PodatakFacade.class.getName());
@@ -47,11 +50,28 @@ public class PodatakFacade extends AbstractFacade<Podatak> {
         Root<IzvorPodataka> from = cq.from(IzvorPodataka.class);
         cq.where(cb.equal(from.get(IzvorPodataka_.aktivan), true));
         cq.select(from);
-        return  em.createQuery(cq).getResultList();
+        return em.createQuery(cq).getResultList();
     }
 
     public Map<ProgramMjerenja, Podatak> getZadnjiPodatakPoProgramu() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+        Root<Podatak> from = cq.from(Podatak.class);
 
+        Expression<NivoValidacije> nivoValidacijeE = from.get(Podatak_.nivoValidacijeId);
+        Expression<Date> vrijeme = from.get(Podatak_.vrijeme);
+        Expression<ProgramMjerenja> program = from.get(Podatak_.programMjerenjaId);
+
+        cq.where(cb.equal(nivoValidacijeE, new NivoValidacije((short) 0)));
+        cq.groupBy(program);
+        cq.multiselect(cb.greatest(vrijeme), from);
+
+        Map<ProgramMjerenja, Podatak> pm = new HashMap<>();
+        for (Tuple t : em.createQuery(cq).getResultList()) {
+            Date vr = t.get(0, Date.class);
+            Podatak po = t.get(1, Podatak.class);
+            pm.put(po.getProgramMjerenjaId(), po);
+        }
+        return pm;
+    }
 }
